8cc13a16c251b14631c287b30eec7e49250bfe73
Jonathan Reiter <reiterj@protonmail.com>
2016-06-04 05:28:52 -0400

    rev: 1

--- guerra/main.cpp
+++ guerra/main.cpp
@@ -2,14 +2,18 @@
 //  main.cpp
 //  guerra
 //
+//	Rev: 1
 //  Created by Jon Reiter on 6/4/16.
 //  Copyright © 2016 Jon Reiter. All rights reserved.
 //
 
 #include <iostream>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+using namespace std;
+
+
+int main() {
+
     return 0;
 }
