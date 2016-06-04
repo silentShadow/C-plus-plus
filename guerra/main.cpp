ca1b563ecfb035e2192883f36f0a858fc4cd3c04
Jonathan Reiter <reiterj@protonmail.com>
2016-06-04 05:18:43 -0400

    initial commit

---
+++ guerra/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  guerra
+//
+//  Created by Jon Reiter on 6/4/16.
+//  Copyright © 2016 Jon Reiter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
